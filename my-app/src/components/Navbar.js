867bbb21cb38250c02bbbf8beaa5a741ce697954
lamdiki-sherpa <lamdikisherpa911@gmail.com>
2023-03-19 19:47:51 +0545

    first commit

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,13 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../App.css'
+////////////////NavLink allow us to style
 const Navbar = () => {
   return (
     <nav className='navbar'>
-        <Link to={"/"} className='navbar-item'>Home</Link>
-        <Link to={"/about"} className='navbar-item'>About</Link>
-        <Link to={"/products"} className='navbar-item'>Products</Link>
-        <Link to={"/login"} className='navbar-item'>Login</Link>
+        {/* <NavLink to={"/"} style={({isActive})=>{
+            return {color:isActive ? 'red' : 'green' }
+
+        }}>Home</NavLink> ==================we can either use style or classname=========*/}
+         <NavLink to={"/"} className={({isActive})=>isActive ? 'active' : 'link' }
+
+        >Home</NavLink>
+        <NavLink to={"/about"} className={({isActive})=>isActive ? ' active' : 'link' 
+
+        }>About</NavLink>
+        <NavLink to={"/products"} className={({isActive})=>isActive ? ' active' : 'link' 
+
+        }>Products</NavLink>
+         <NavLink to={"/login"} className={({isActive})=>isActive ? ' active' : 'link' 
+
+}>Login</NavLink>
     </nav>
   )
 }
